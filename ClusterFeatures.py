9a65efcad9a335a1871d554aeb51a5063c6206a2
Luca Tesei <lucatesei@Air-di-Luca.homenet.telecomitalia.it>
2022-08-31 17:30:44 +0200

    Adjusted Python cripts. Uploaded 16S 23S and 5S rRNAs experiment

--- ClusterFeatures.py
+++ ClusterFeatures.py
@@ -1,7 +1,26 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-EvalTax: executes the evaluation of 
+TaxonClassifier: execute agglomerative clustering on a set of molecules based on
+a pair of features and evaluate the quality of the clustering w.r.t. a known 
+clusterization.
+
+Usage: python3 ClusterFeatures.py <molecule-list-csv-file> <eigenvalues-csv-file>
+
+The format of <molecule-list-csv-file> must be <"Id", "Organism", "Taxon"> where
+Id is a unique identifier in the file, Organism is a textual description of the 
+Id and Taxon is the label associated to Id by a known classification
+
+The format of <eigenvalues-csv-file> must be <"Id", "valueS", "valueS"> where Id
+is one Id of the first file and valueS/valueE are features computed for each Id.
+The features are used to 
+
+The output is given textually as the values of "Rand_score", "Homogeneity_score" 
+and "Completeness_score" metrics computed for each executed clustering and for 
+each linkage parameter of the clustering algorithm: single, complete and 
+average.
+
+
 @author: Michela Quadrini and Luca Tesei
 """
 import math
@@ -9,64 +28,43 @@ import sys
 import pandas as pd
 import numpy as np
 from sklearn.metrics import *
-
 from sklearn.cluster import *
 from sklearn import metrics
-
 import argparse
-if len(sys.argv) != 3 :
-    print("Usage: python3 prove.py <molecule-list-csv-file> <eigenvalues-csv-file>")
-    sys.exit(1)
-# Read the list of molecules
-#print("Reading", sys.argv[1], "...")
-molecules = pd.read_csv(sys.argv[1], sep=";")
-#print(molecules)
-#molecules.columns = ["Id", "Organism", "Taxon", "Label"]
-#print(molecules.columns)
 
-#print(len(molecules))
-#print(molecules['Id'][0])
+if len(sys.argv) != 3 :
+    print("Usage: python3 ClusterFeatures.py <molecule-list-csv-file> <eigenvalues-csv-file>")
+    sys.exit(1)
+
+# Read the list of molecules
+molecules = pd.read_csv(sys.argv[1], sep=";")
 
 # Create dictionary Id -> Index
 index_of = dict()
 for i in range(len(molecules)) :
     index_of[molecules.loc[i].loc['Id']] = i
-#print(index_of)
-#print(index_of["CRW_16S_A_E_9_noHeader-2D-dotbracket.txt"])
 
 # Create dictionary Id -> Organism 
 organism_of = dict()
 for i in range(len(molecules)) :
     organism_of[molecules.loc[i].loc['Id']] = molecules.loc[i].loc['Organism'].strip()
-#print(organism_of)
-#print(organism_of["CRW_16S_A_E_9_noHeader-2D-dotbracket.txt"])
 
 # Create dictionary Id -> Taxon 
 taxon_of = dict()
 for i in range(len(molecules)) :
     taxon_of[molecules.loc[i].loc['Id']] = molecules.loc[i].loc['Taxon'].strip()
-#print(taxon_of)
-#print(taxon_of["CRW_16S_A_E_9_noHeader-2D-dotbracket.txt"])
 
 # Create dictionary Id -> Label 
 label_of = dict()
 for i in range(len(molecules)) :
     label_of[molecules.loc[i].loc['Id']] = molecules.loc[i].loc['Taxon'].strip()
-#print(label_of)
-#print(label_of["CRW_16S_A_E_9_noHeader-2D-dotbracket.txt"])
 
-# Read the list of distances
+# Read the list of features
 print("Reading", sys.argv[2], "...")
 distances = pd.read_csv(sys.argv[2], sep=";")
 
-#print(distances)
 distances.columns = ["Id1", "ValueS", "ValueE"]
 print(distances.columns)
-
-
-# Create Distance Matrix
-#s= (len(molecules),len(molecules))
-#distance_matrix= np.zeros(s)
 
 # Populate List of Features
 ListFeatures =[]
@@ -77,46 +75,47 @@ for k in range(len(distances)) :
     value2 = distances.loc[k].loc['ValueE']
     ListFeatures.append([value1,value2]) 
 
-#print(ListFeatures)
-
 # Determine the number of clusters as distinct labels in molecules
-
-#print(label_of.values())
-#print(type(label_of.values()))
 n_clusters = len(set(label_of.values()))
-#print(n_clusters)
 
+# Read the true lables assigned to every Id 
 labels_true = list(label_of.values())
-#print("Labels True",labels_true)
 
-# Parameter linkage can be varied to obtain clusters differently
-# Options are: single, complete, average, ward (but ward works only if Euclian distance is used)
+# Execute clustering with single linkage and determines the predicted labels for each molecule. In this case features are used and the Euclidean distance is chosen as affinity.
 
 model = AgglomerativeClustering(n_clusters=n_clusters, affinity='euclidean', linkage ='single').fit(ListFeatures)
 labels_pred = model.fit_predict(ListFeatures)
-#print("Labels Pred", list(labels_pred))
 
+# Compute the metrics and print the evaluations
 print("Method: single")
+#print("Labels Pred", list(labels_pred))
+#print("Labels True", list(labels_true))
 print("Rand_score", metrics.rand_score(labels_true, labels_pred))
 print("Homogeneity_score", metrics.homogeneity_score(labels_true, labels_pred))
 print("completeness_score", metrics.completeness_score(labels_true, labels_pred))
 
+# Execute clustering with complete linkage and determines the predicted labels for each molecule. In this case features are used and the Euclidean distance is chosen as affinity.
 
 model = AgglomerativeClustering(n_clusters=n_clusters, affinity='euclidean', linkage ='complete').fit(ListFeatures)
 labels_pred = model.fit_predict(ListFeatures)
-#print("Labels Pred", list(labels_pred))
 
+# Compute the metrics and print the evaluations
 print("Method: complete")
+#print("Labels Pred", list(labels_pred))
+#print("Labels True", list(labels_true))
 print("Rand_score", metrics.rand_score(labels_true, labels_pred))
 print("Homogeneity_score", metrics.homogeneity_score(labels_true, labels_pred))
 print("completeness_score", metrics.completeness_score(labels_true, labels_pred))
 
+# Execute clustering with average linkage and determines the predicted labels for each molecule. In this case features are used and the Euclidean distance is chosen as affinity.
 
 model = AgglomerativeClustering(n_clusters=n_clusters, affinity='euclidean', linkage ='average').fit(ListFeatures)
 labels_pred = model.fit_predict(ListFeatures)
-#print("Labels Pred", list(labels_pred))
 
+# Compute the metrics and print the evaluations
 print("Method: average")
+#print("Labels Pred", list(labels_pred))
+#print("Labels True", list(labels_true))
 print("Rand_score", metrics.rand_score(labels_true, labels_pred))
 print("Homogeneity_score", metrics.homogeneity_score(labels_true, labels_pred))
 print("completeness_score", metrics.completeness_score(labels_true, labels_pred))
--- ClusterMatrix.py
+++ ClusterMatrix.py
@@ -1,7 +1,26 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-EvalTax: executes the evaluation of 
+TaxonClassifier: execute agglomerative clustering on a set of molecules based on
+a distance matrix and evaluate the quality of the clustering w.r.t. a known 
+clusterization.
+
+Usage: python3 ClusterMatrix.py <molecule-list-csv-file> <distances-csv-file>
+
+The format of <molecule-list-csv-file> must be <"Id", "Organism", "Taxon"> where
+Id is a unique identifier in the file, Organism is a textual description of the 
+Id and Taxon is the label associated to Id by a known classification
+
+The format of <distances-csv-file> must be <"Id1", "Id2", "Distance"> where Id1 
+and Id2 are two different Ids of the first file and Distance is a floating point
+value corresponding to the distance from Id1 and Id2 computed by a chosen 
+comparison method. 
+
+The output is given textually as the values of "Rand_score", "Homogeneity_score" 
+and "Completeness_score" metrics computed for each executed clustering and for 
+each linkage parameter of the clustering algorithm: single, complete and 
+average.
+
 @author: Michela Quadrini and Luca Tesei
 """
 import math
@@ -9,58 +28,34 @@ import sys
 import pandas as pd
 import numpy as np
 from sklearn.metrics import *
-
 from sklearn.cluster import *
 from sklearn import metrics
-
 import argparse
+
 if len(sys.argv) != 3 :
-    print("Usage: python3 prove.py <molecule-list-csv-file> <distances-csv-file>")
+    print("Usage: python3 ClusterMatrix.py <molecule-list-csv-file> <distances-csv-file>")
     sys.exit(1)
 # Read the list of molecules
-#print("Reading", sys.argv[1], "...")
 molecules = pd.read_csv(sys.argv[1], sep=";")
-#print(molecules)
-#molecules.columns = ["Id", "Organism", "Taxon", "Label"]
-#print(molecules.columns)
-
-#print(len(molecules))
-#print(molecules['Id'][0])
 
 # Create dictionary Id -> Index
 index_of = dict()
 for i in range(len(molecules)) :
     index_of[molecules.loc[i].loc['Id']] = i
-#print(index_of)
-#print(index_of["CRW_16S_A_E_9_noHeader-2D-dotbracket.txt"])
 
 # Create dictionary Id -> Organism 
 organism_of = dict()
 for i in range(len(molecules)) :
     organism_of[molecules.loc[i].loc['Id']] = molecules.loc[i].loc['Organism'].strip()
-#print(organism_of)
-#print(organism_of["CRW_16S_A_E_9_noHeader-2D-dotbracket.txt"])
-
-# Create dictionary Id -> Taxon 
-taxon_of = dict()
-for i in range(len(molecules)) :
-    taxon_of[molecules.loc[i].loc['Id']] = molecules.loc[i].loc['Taxon'].strip()
-#print(taxon_of)
-#print(taxon_of["CRW_16S_A_E_9_noHeader-2D-dotbracket.txt"])
 
 # Create dictionary Id -> Label 
 label_of = dict()
 for i in range(len(molecules)) :
     label_of[molecules.loc[i].loc['Id']] = molecules.loc[i].loc['Taxon'].strip()
-#print(label_of)
-#print(label_of["CRW_16S_A_E_9_noHeader-2D-dotbracket.txt"])
 
 # Read the list of distances
 print("Reading", sys.argv[2], "...")
 distances = pd.read_csv(sys.argv[2], sep=";")
-#print(distances)
-#distances.columns = ["Id1", "Id2", "Distance"]
-#print(distances.columns)
 
 # Create Distance Matrix
 s= (len(molecules),len(molecules))
@@ -74,46 +69,48 @@ for k in range(len(distances)) :
     distance_matrix[i][j] = value
     distance_matrix[j][i] = value
 
-#print(distance_matrix)
-
 # Determine the number of clusters as distinct labels in molecules
-
-#print(label_of.values())
-#print(type(label_of.values()))
 n_clusters = len(set(label_of.values()))
-#print(n_clusters)
 
+# Read the true lables assigned to every Id 
 labels_true = list(label_of.values())
-#print("Labels True",labels_true)
 
-# Parameter linkage can be varied to obtain clusters differently
-# Options are: single, complete, average, ward (but ward works only if Euclian distance is used)
+# Execute clustering with single linkage and determines the predicted labels for each molecule
 
 model = AgglomerativeClustering(n_clusters=n_clusters, affinity='precomputed', linkage ='single').fit(distance_matrix)
 labels_pred = model.fit_predict(distance_matrix)
-#print("Labels Pred", list(labels_pred))
 
+# Compute the metrics and print the evaluations
 print("Method: single")
+#print("Labels Pred", list(labels_pred))
+#print("Labels True", list(labels_true))
 print("Rand_score", metrics.rand_score(labels_true, labels_pred))
 print("Homogeneity_score", metrics.homogeneity_score(labels_true, labels_pred))
-print("completeness_score", metrics.completeness_score(labels_true, labels_pred))
+print("Completeness_score", metrics.completeness_score(labels_true, labels_pred))
     
+# Execute clustering with complete linkage and determines the predicted labels for each molecule
 
-print("Method: complete")
 model = AgglomerativeClustering(n_clusters=n_clusters, affinity='precomputed', linkage ='complete').fit(distance_matrix)
 labels_pred = model.fit_predict(distance_matrix)
-#print("Labels Pred", list(labels_pred))
 
+# Compute the metrics and print the evaluations
+print("Method: complete")
+#print("Labels Pred", list(labels_pred))
+#print("Labels True", list(labels_true))
 print("Rand_score", metrics.rand_score(labels_true, labels_pred))
 print("Homogeneity_score", metrics.homogeneity_score(labels_true, labels_pred))
-print("completeness_score", metrics.completeness_score(labels_true, labels_pred))
+print("Completeness_score", metrics.completeness_score(labels_true, labels_pred))
 
-print("Method: average")
+
+# Execute clustering with average linkage and determines the predicted labels for each molecule
+
 model = AgglomerativeClustering(n_clusters=n_clusters, affinity='precomputed', linkage ='average').fit(distance_matrix)
 labels_pred = model.fit_predict(distance_matrix)
-#print("Labels Pred", list(labels_pred))
 
+# Compute the metrics and print the evaluations
+print("Method: average")
+#print("Labels Pred", list(labels_pred))
+#print("Labels True", list(labels_true))
 print("Rand_score", metrics.rand_score(labels_true, labels_pred))
 print("Homogeneity_score", metrics.homogeneity_score(labels_true, labels_pred))
-print("completeness_score", metrics.completeness_score(labels_true, labels_pred))
-#print("Labels Pred", list(labels_pred))
+print("Completeness_score", metrics.completeness_score(labels_true, labels_pred))
